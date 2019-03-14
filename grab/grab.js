ea7171199374fa5b219fb34de5790b8d660e8941
Srikanth <srikanthperi@outlook.com>
2019-03-14 14:15:56 +0530

    Updated grab.js with Win updates

--- grab/grab.js
+++ grab/grab.js
@@ -1,26 +1,47 @@
 const
 	entities = {
-		Products: {
-			tokens: 4
-		},
-		Suppliers: {
-			tokens: 1
-		},
-		Categories: {
-			tokens: 1
-		}
+				Products:   {  tokens: 4 },
+				Suppliers:  { tokens: 1 },
+				Categories: {
+					tokens: 1,
+					fields: [
+							{ label: 'ID'					, value: 'CategoryID'},
+							{ label: 'name'			  , value: 'CategoryName'},
+							{ label: 'description', value: 'Description'},
+					]
+				}
 	},
 	axios = require('axios'),
-	baseurl = 'http://localhost:8000/Products-',
+  fs = require('fs'),
+	json2csv = require('json2csv').parse,
+
+	buildurl = (entity, n) =>
+			'http://localhost:8000/ENTITY-TOKEN'
+			.replace(/ENTITY/, entity)
+			.replace(/TOKEN/, n*20)
+
 	range = x => [...Array(x).keys()],
+
+	// is = (val, x) => x === val,
+	is = val => x => x === val,
+	onlyCategories = is('Categories'),
+
 	grab = entity => axios
-						.all(range(entities[entity].tokens + 1).map(x => axios.get(baseurl + (x*20))))
+						.all(range(entities[entity].tokens).map(x => axios.get(buildurl(entity,x))))
 						.then(xs => xs.reduce((a, x) => a.concat(x.data.value), []))
 
 Object
 	.keys(entities)
-	.forEach(entity => grab(entity).then(console.log))
-// .forEach(entity => console.log(grab(entity)))
+	.filter(onlyCategories)
+	// .filter(is(Categories))
+	.forEach(entity => grab(entity)
+				// .then(xs => xs.length)
+				.then(xs => json2csv(xs, { fields: entities[entity].fields }))
+				// .then(JSON.stringify)
+				// .then(console.log)
+				.then(xs => fs.writeFileSync('data/' + entity + '.csv', xs))
+	)
+	 // .forEach(entity => console.log(grab(entity)))
 //grab('Products')
 //	.then(xs => xs.length)
 //	.then(console.log)
